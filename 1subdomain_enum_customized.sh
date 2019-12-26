d3b8a65d66d5efe13fff589e689b8791b673a02e
offsecdawn <offsecdawn@gmail.com>
2019-12-26 05:25:19 +0000

    first commit

---
+++ 1subdomain_enum_customized.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo $1
+source ~/.bash_profile
+for i in $(cat domains.txt)
+do
+	#crtsh %25.$i
+	crtsh %25$i
+done
+#crtsh $1.%25
+#crtsh %25admin$25.$1
+#crtsh %25api%25.$1
+#crtsh %25dev%25.$1
+#crtsh %25corp%25.$1
+#crtsh %25interanl%25.$1
+#crtsh %25payment%25.$1
+#crtsh %25stg%25.$1
